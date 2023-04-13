0fb8c68fa6f3b457defd56ac644a8cf364e48ee7
Brian Emilius <brian@brianemilius.com>
2023-04-13 14:14:08 +0200

    patch paths

--- slides.js
+++ slides.js
@@ -1,34 +1,34 @@
 const slides = [
 	{
-		src: "/images/cheese1.webp",
+		src: "./images/cheese1.webp",
 		heading: "Slide 1",
 		body: "yummeh yummeh",
 		buttonText: "KLIK HER!!",
 		buttonLink: "https://google.com"
 	},
 	{
-		src: "/images/cheese2.jpg",
+		src: "./images/cheese2.jpg",
 		heading: "Slide 2",
 		body: "yummeh yummeh",
 		buttonText: "KLIK HER!!",
 		buttonLink: "https://google.com"
 	},
 	{
-		src: "/images/cheese3.jpg",
+		src: "./images/cheese3.jpg",
 		heading: "Slide 3",
 		body: "yummeh yummeh",
 		buttonText: "KLIK HER!!",
 		buttonLink: "https://google.com"
 	},
 	{
-		src: "/images/cheese4.jpg",
+		src: "./images/cheese4.jpg",
 		heading: "Slide 4",
 		body: "yummeh yummeh",
 		buttonText: "KLIK HER!!",
 		buttonLink: "https://google.com"
 	},
 	{
-		src: "/images/cheese5.jpg",
+		src: "./images/cheese5.jpg",
 		heading: "Slide 5",
 		body: "yummeh yummeh",
 		buttonText: "KLIK HER!!",
